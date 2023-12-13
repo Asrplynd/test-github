1568c13c73471188c09ba2e86dfaf1cd7d18a907
Asrplynd <asrafgepelinofficial@gmail.com>
2023-12-13 09:47:31 +0800

    Add addition function to main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 print("Hello World")
 
 print("my Name is asr")
+
+# Function to add two numbers
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+
+# Input numbers
+number1 = 5
+number2 = 3
+
+# Call the function and print the result
+print("The sum is:", add_numbers(number1, number2))
